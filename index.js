9b92670d71a180d9dd5c49b2cb7640873cf86b27
Ty Thompson <tylermoko@gmail.com>
2018-02-02 15:39:15 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+var words = ["cat","dog","french","dude",
+"hamburger","eagle","america","bear","beer","water"];
+var curWord = words[getRandomInt(0,words.length)].split("");
+var blanks = getBlanks(curWord);
+var lives = 5;
+var alreadyGuessed = [];
+var blanksGame = document.querySelector('.game__blanks');
+var userGuess = document.querySelector('.game__input');
+var livesCounter = document.querySelector('.lives__counter');
+var guessButton = document.querySelector('.game__guess');
+var resetButton = document.querySelector('.reset');
+var hintButton = document.querySelector('.hint');
+
+hintButton.addEventListener("click", function() {
+    hint();
+})
+
+resetButton.addEventListener("click", function() {
+    location.reload();
+})
+
+guessButton.addEventListener("click", function(){
+    console.log(checkGuess(userGuess.value,curWord));
+    if(checkGuess(userGuess.value,curWord)) {
+        updateBlanks(userGuess.value,curWord,blanks);
+        updateHTML(blanks,lives);
+    } else {
+        lives--;
+        updateHTML(blanks,lives);
+    }
+    alreadyGuessed.push(userGuess.value);
+    winLose(blanks,lives);
+    userGuess.value = "";
+});
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getBlanks(word) {
+    let blanks = word.map(() => '_');
+      return blanks;
+}
+
+function updateBlanks(guess,word,blanks) {
+    for(let i = 0; i < word.length; i++) {
+        if(guess === word[i]) {
+            blanks[i] = guess;
+        }
+    }
+}
+
+function updateHTML(blanks,lives) {
+    blanksGame.innerHTML = "";
+    for(let i = 0; i < blanks.length; i++) {
+        blanksGame.innerHTML += blanks[i] + " ";
+    }
+    livesCounter.innerHTML = lives;
+}
+
+function checkGuess(guess,word) {
+    if(word.indexOf(guess) >= 0) {
+        return true;
+    }
+    return false;
+}
+
+function winLose(blanks,lives) {
+    if(lives === 0) {
+        lost();
+    }
+    if(blanks.indexOf("_") < 0) {
+        win();
+    }
+}
+
+function lost() {
+    blanksGame.style.fontSize = '3em';
+    blanksGame.innerHTML = "You lose. Want to play again?"
+}
+function win() {
+    blanksGame.style.fontSize = '3em';
+    blanksGame.innerHTML = "You win! Want to play again?"
+}
+
+function hint() {
+    var hintLetter = curWord[getRandomInt(0,curWord.length)];
+    if(alreadyGuessed.indexOf(hintLetter) < 0) {
+    console.log('hint letter:', hintLetter, "curWord:", curWord);
+    alreadyGuessed.push(hintLetter);
+    updateBlanks(hintLetter,curWord,blanks);
+    lives--;
+    updateHTML(blanks,lives);
+    winLose(blanks,lives);
+    } else {
+        hint();
+    }
+}
+updateHTML(blanks,lives);
